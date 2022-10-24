848db82a8a4d085f7ee245d188f6a4e9873a12f2
NurZhans10 <103447872+NurZhans10@users.noreply.github.com>
2022-10-24 16:56:58 +0600

    lab9

---
+++ main.js
@@ -0,0 +1,33 @@
+//3. Write a function that finds an area of a circle given its radius 
+  
+let a=3
+let circle_area=circle(a);
+function circle (rad) {
+	result = 3.14* rad*rad
+	return result
+}
+
+const results = {
+	circle_area,
+}
+console.log("area of a circle with radius 3 :",results);
+
+
+//10. Write a function to check whether two given numbers are equal or not
+let x=5;
+let y=8;
+if (x==y) {
+	console.log ("numbers 5 and 8 are equal")
+	}
+else {
+	console.log ("numbers 5 and 8 are not equal")
+}
+
+// 11.Write a function to check whether a given number is even or odd.
+let z=11;
+if (z%2==0) {
+	console.log ("number 11 is even")
+	}
+else {
+	console.log ("number 11 is odd")
+}
